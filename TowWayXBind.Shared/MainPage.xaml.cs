952eb68e56cbda3687046f18c50353e691126aef
Kazuki Ota <kaota@microsoft.com>
2020-02-03 16:01:11 +0900

    プロジェクト ファイルを追加します。

---
+++ TowWayXBind.Shared/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace TowWayXBind
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainPageViewModel ViewModel { get; } = new MainPageViewModel();
+        public MainPage()
+        {
+            DataContext = ViewModel;
+            InitializeComponent();
+        }
+    }
+
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _message = "initial value";
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                if (_message == value)
+                {
+                    return;
+                }
+
+                _message = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Message)));
+            }
+        }
+    }
+}
